caf1e326ef3909eabbc72e9402913d0aae7ca280
Micale <gmgmicky@gmail.com>
2017-07-31 13:20:49 +0200

    initial pvalues code

---
+++ src/main/java/org/biiig/dmgm/tfsm/dmg_gspan/pvalues/CalculatePValue.java
@@ -0,0 +1,76 @@
+package org.biiig.dmgm.tfsm.dmg_gspan.pvalues;
+
+import org.biiig.dmgm.tfsm.dmg_gspan.model.countable.Countable;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.algorithm.MathUtility;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.algorithm.Occurrence;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.model.MultiEdgeLabelDistributionKey;
+import org.biiig.dmgm.tfsm.dmg_gspan.gspan.DFSCode;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.algorithm.EDDModel;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.algorithm.GraphUtility;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.algorithm.PolyaAeppli;
+import org.biiig.dmgm.tfsm.dmg_gspan.pvalues.model.VertexDegreeKey;
+
+import java.util.Map;
+
+public class CalculatePValue {
+
+  private final Map<VertexDegreeKey, Map<Long, Float>> vertexDegreeDistribution;
+  private final Map<MultiEdgeLabelDistributionKey, Float> edgeLabelDistribution;
+  private final Map<String, Float> vertexLabelDistribution;
+  private final Long globalVertexCount;
+  private final Integer maxMultiplicity;
+  private final GraphUtility gu = new GraphUtility();
+  private final Map<Integer, String> vertexDictionary;
+  private final Map<Integer, String> edgeDictionary;
+
+
+  public CalculatePValue(Map<VertexDegreeKey, Map<Long, Float>> vertexDegreeDistribution,
+    Map<MultiEdgeLabelDistributionKey, Float> edgeLabelDistribution,
+    Map<String, Float> vertexLabelDistribution, Long globalVertexCount, Integer maxMultiplicity,
+    Map<Integer, String> vertexDictionary, Map<Integer, String> edgeDictionary) {
+    this.vertexDegreeDistribution = vertexDegreeDistribution;
+    this.edgeLabelDistribution = edgeLabelDistribution;
+    this.vertexLabelDistribution = vertexLabelDistribution;
+    this.globalVertexCount = globalVertexCount;
+    this.maxMultiplicity = maxMultiplicity;
+    this.vertexDictionary = vertexDictionary;
+    this.edgeDictionary = edgeDictionary;
+  }
+
+  public double calculate(Countable<DFSCode> countable) {
+
+    long numberOfOccurrences = countable.getFrequency();
+    Occurrence occurrence = gu.toOccurrence(countable.getObject(), vertexDictionary, edgeDictionary);
+    int vertexCount = occurrence.getNodeLabels().length;
+
+    EDDModel edd = new EDDModel(
+      vertexDegreeDistribution,
+      edgeLabelDistribution,
+      vertexLabelDistribution,
+      this.globalVertexCount,
+      maxMultiplicity,
+      vertexCount
+    );
+
+    PolyaAeppli pa = new PolyaAeppli();
+    double pValue;
+    double numAutos = gu.findAutomorphisms(occurrence);
+    double numNRP = MathUtility.factorial(vertexCount) / numAutos;
+    double meanAnal = edd.computeMean(occurrence, numNRP);
+    /*System.out.println("Num of occs: "+numberOfOccurrences);
+    System.out.println("Mean anal: "+meanAnal);*/
+    pValue = pa.estimateMaxPvalue(numberOfOccurrences, meanAnal);
+
+    if (pValue < -1.0E-17) {
+      pValue = 0.0;
+    } else if (pValue > 1.0) {
+      pValue = 1.0;
+    }
+    //System.out.println("MotifSize: "+vertexCount+"\tfactorial: "+MathUtility.factorial(vertexCount)
+    // +"\tnumAutos: "+numAutos+"\tnrp: "+numNRP+"\tPVALUE: "+pValue);
+
+//    System.out.println("PVALUE: " + pValue);
+
+    return pValue;
+  }
+}
